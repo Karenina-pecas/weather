277f282c12fb49181c04724436a9be6fe4b336b7
Karenina <arq.karengallegos89@gmail.com>
2021-09-15 14:45:32 +0200

    fixing

--- client/src/components/Icon.js
+++ client/src/components/Icon.js
@@ -39,7 +39,7 @@ export default function Icon({ condition }) {
 	}
 	return (
 		<div>
-			<Icon className='icon' src={process.env.PUBLIC_URL + `/weather/images/${icon}`} alt='Weather Icon' />
+			<Icon className='icon' src={process.env.PUBLIC_URL + `/images/${icon}`} alt='Weather Icon' />
 		</div>
 	);
 }
